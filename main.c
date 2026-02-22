52f59098cefac8d3774e1205efa4910745efccbd
Alfie432 <alphaoshawa@gmail.com>
2026-02-22 16:29:20 -0500

    setup the main.c file

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+// macros
+
+
+// define the contact struct, each contact will have this info stored in the contacts file
+typedef struct Contact
+{
+    char name[24];
+    char email[32];
+    char phone[16];
+} Contact;
+
+char filename[] = "contacts.txt";
+FILE *file; // declare the file here, so it can be accessed anywhere
+
+// functions prototypes
+
 int main(void)
 {   
+
 
     return 0;
 }
